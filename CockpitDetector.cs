5e0254e7358c7ef11906af9add2c708efb2d9c0a
Etherpod <etherpod@gmail.com>
2024-03-19 21:43:41 -0700

    Cockpit puzzle

---
+++ CockpitDetector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace OWModJam;
+
+[RequireComponent(typeof(OWTriggerVolume))]
+public class CockpitDetector : MonoBehaviour
+{
+	OWTriggerVolume trigger;
+
+	private void Start()
+	{
+		ModMain.Instance.ModHelper.Console.WriteLine("Start");
+        trigger = GetComponent<OWTriggerVolume>();
+		trigger.OnEntry += OnEntry;
+		trigger.OnExit += OnExit;
+	}
+
+	private void OnEntry(GameObject hitObj)
+	{
+        if (hitObj.transform.parent.name == "Module_Cockpit_Body")
+		{
+			//ModMain.Instance.ModHelper.Console.WriteLine("Detected cockpit", OWML.Common.MessageType.Info);
+			DialogueConditionManager.SharedInstance.SetConditionState("GOT_COCKPIT", true);
+		}
+	}
+
+	private void OnExit(GameObject hitObj)
+	{
+        if (hitObj.transform.parent.name == "Module_Cockpit_Body")
+        {
+            //ModMain.Instance.ModHelper.Console.WriteLine("Detected cockpit", OWML.Common.MessageType.Info);
+            DialogueConditionManager.SharedInstance.SetConditionState("GOT_COCKPIT", true);
+        }
+    }
+}
---
+++ ModMain.cs
@@ -0,0 +1,31 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OWModJam;
+public class ModMain : ModBehaviour
+{
+    public static ModMain Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        // Starting here, you'll have access to OWML's mod helper.
+        //ModHelper.Console.WriteLine($"My mod {nameof(ModMain)} is loaded!", MessageType.Success);
+
+        // Get the New Horizons API and load configs
+        var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+        newHorizons.LoadConfigs(this);
+
+        // Example of accessing game code.
+        LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+        {
+            if (loadScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        };
+    }
+}
+
