4260ff6bba8727d850d654410be69880803dbbc9
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2024-03-24 14:16:34 -0500

    added code

--- ModMain.cs
+++ ModMain.cs
@@ -1,6 +1,7 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 namespace BandTogether;
@@ -32,6 +33,32 @@ public class ModMain : ModBehaviour
         {
             if (loadScene != OWScene.SolarSystem) return;
             ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+
+            ModHelper.Events.Unity.FireInNUpdates(() =>
+            {
+                var relativeLocation = new RelativeLocationData(Vector3.up * 2 + Vector3.forward * 2, Quaternion.identity, Vector3.zero);
+
+                //nessesary for owlks, lets them work somehow????????
+                var location = DreamArrivalPoint.Location.Zone3;
+                var arrivalPoint = Locator.GetDreamArrivalPoint(location);
+                var dreamCampfire = Locator.GetDreamCampfire(location);
+                if (Locator.GetToolModeSwapper().GetItemCarryTool().GetHeldItemType() != ItemType.DreamLantern)
+                {
+                    var dreamLanternItem = GameObject.Find("THE_GOD_OF_ALL_LANTERNS_PIKPIK_CARROT").GetComponent<DreamLanternItem>();
+                    dreamLanternItem.OnEnterDreamWorld();
+                    Locator.GetDreamWorldController()._playerLantern = dreamLanternItem;
+                };
+
+                //Puts player into a semi-dreamworld state
+                Locator.GetDreamWorldController().EnterDreamWorld(dreamCampfire, arrivalPoint, relativeLocation);
+                Resources.FindObjectsOfTypeAll<GhostBrain>().ToList().ForEach((brain) =>
+                {
+                    brain.enabled = true;
+                    brain.WakeUp();
+                    brain.OnEnterDreamWorld();
+                });
+            }, 15);
+
         };
     }
 
---
+++ MyPatchClass.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandTogether
+{
+    internal class MyPatchClass
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GhostSensors), nameof(GhostSensors.FixedUpdate_Sensors))]
+        public static void FlashLightOwlk(GhostSensors __instance)
+        {
+            __instance._data.sensor.isIlluminatedByPlayer = __instance._data.sensor.isIlluminated;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GhostGrabController), nameof(GhostGrabController.OnSnapPlayerNeck))]
+        public static void OwlkSnap(GhostGrabController __instance)
+        {
+            if (!Locator.GetDeathManager().IsPlayerDying() && !Locator.GetDeathManager().IsPlayerDead())
+            {
+                Locator.GetDeathManager().KillPlayer(DeathType.CrushedByElevator);
+            }
+        }
+    }
+}
