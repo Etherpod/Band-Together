0202a0453aad4133098620f9efc18d9e31fd4b3a
Etherpod <etherpod@gmail.com>
2024-03-26 00:39:45 -0700

    Fixed shrub socket

--- TheDivineThrone.cs
+++ TheDivineThrone.cs
@@ -4,26 +4,27 @@ namespace BandTogether;
 
 public class TheDivineThrone : OWItemSocket
 {
-  public override void Awake()
-  {
-    base.Awake();
-    _acceptableType = Shrubbery.ItemType;
-  }
-  
-  public override void Start()
-  {
-    base.Start();
+    public override void Awake()
+    {
+        base.Awake();
+        _acceptableType = Shrubbery.ItemType;
+    }
 
-    OnSocketableDonePlacing += OnShrubberyReSeatedUponItsHolyArborealThrone;
-  }
+    public override void Start()
+    {
+        base.Start();
 
-  private void OnDestroy()
-  {
-    OnSocketableDonePlacing -= OnShrubberyReSeatedUponItsHolyArborealThrone;
-  }
+        OnSocketableDonePlacing += OnShrubberyReSeatedUponItsHolyArborealThrone;
+    }
 
-  private void OnShrubberyReSeatedUponItsHolyArborealThrone(OWItem item)
-  {
-    
-  }
+    private void OnDestroy()
+    {
+        OnSocketableDonePlacing -= OnShrubberyReSeatedUponItsHolyArborealThrone;
+    }
+
+    private void OnShrubberyReSeatedUponItsHolyArborealThrone(OWItem item)
+    {
+        base.EnableInteraction(false);
+        DialogueConditionManager.SharedInstance.SetConditionState("FINISH_SHRUB_QUEST", true);
+    }
 }
