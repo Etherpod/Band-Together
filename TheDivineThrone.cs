d690b3be48411fa82ebc06c24f2ff5715b9067b2
ditzy <ditzy.zy@pm.me>
2024-03-26 00:50:02 -0500

    add the divine throne

---
+++ TheDivineThrone.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace BandTogether;
+
+public class TheDivineThrone : OWItemSocket
+{
+  public override void Awake()
+  {
+    base.Awake();
+    _acceptableType = Shrubbery.ItemType;
+  }
+  
+  public override void Start()
+  {
+    base.Start();
+
+    OnSocketableDonePlacing += OnShrubberyReSeatedUponItsHolyArborealThrone;
+  }
+
+  private void OnDestroy()
+  {
+    OnSocketableDonePlacing -= OnShrubberyReSeatedUponItsHolyArborealThrone;
+  }
+
+  private void OnShrubberyReSeatedUponItsHolyArborealThrone(OWItem item)
+  {
+    
+  }
+}
