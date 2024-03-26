0fdef4ee74d9eef258b6bb0b295a681af38e9446
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2024-03-25 23:35:35 -0500

    flashlight ruleset

---
+++ FlashLightRuleset.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BandTogether
+{
+    internal class FlashlightRuleset : MonoBehaviour
+    {
+        [SerializeField]
+        float FillLight = 90;
+
+        [SerializeField]
+        float SpotLight = 50;
+
+        [SerializeField]
+        private Collider _trigger;
+
+        private void OnTriggerEnter(Collider _trigger)
+        {
+            var Flashlight_FillLight = GameObject.Find("Player_Body/PlayerCamera/FlashlightRoot/Flashlight_BasePivot/Flashlight_WobblePivot/Flashlight_FillLight").GetComponent<OWLight2>();
+
+            var Flashlight_SpotLight = GameObject.Find("Player_Body/PlayerCamera/FlashlightRoot/Flashlight_BasePivot/Flashlight_WobblePivot/Flashlight_SpotLight").GetComponent<OWLight2>();
+
+            Flashlight_FillLight.range = FillLight;
+
+            Flashlight_SpotLight.range = SpotLight;
+        }
+        private void OnTriggerExit(Collider _trigger)
+        {
+            var Flashlight_FillLight = GameObject.Find("Player_Body/PlayerCamera/FlashlightRoot/Flashlight_BasePivot/Flashlight_WobblePivot/Flashlight_FillLight").GetComponent<OWLight2>();
+
+            var Flashlight_SpotLight = GameObject.Find("Player_Body/PlayerCamera/FlashlightRoot/Flashlight_BasePivot/Flashlight_WobblePivot/Flashlight_SpotLight").GetComponent<OWLight2>();
+
+            Flashlight_FillLight.range = 90;
+
+            Flashlight_SpotLight.range = 50;
+        }
+    }
+}
