ff2c5d2ac10ff896f508d2407a2395b3ef794b55
Etherpod <etherpod@gmail.com>
2024-04-05 23:32:28 -0700

    Sun gem and dialogue

--- SunpostDetector.cs
+++ SunpostDetector.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BandTogether;
 public class SunpostDetector : MonoBehaviour
@@ -8,42 +6,90 @@ public class SunpostDetector : MonoBehaviour
 	[SerializeField] Transform target;
 	[SerializeField] Transform hole;
 	[SerializeField] EclipseDoorController doorController;
+	[SerializeField] EntrywayTrigger doorEntryway;
+	[SerializeField] OWEmissiveRenderer gemEmissive;
+	[SerializeField] float gemFadeTime;
 
 	Transform sunTransform;
-	bool correctTime;
-	bool opened;
+	bool correctTime = false;
+	bool opened = false;
+	bool waitToClose = false;
+	bool playerInHouse = false;
+
+	float gemFadeStart;
+	float lastGemFade;
+	bool fadeGemComplete = false;
 
 	private void Start()
 	{
         sunTransform = ModMain.Instance.nhAPI.GetPlanet("Jam 3 Sun").transform;
+		gemEmissive.SetEmissiveScale(0f);
+		doorEntryway.OnEntry += OnEntry;
+		doorEntryway.OnExit += OnExit;
     }
 
 	private void Update()
 	{
-		if (opened) return;
-
-		if (sunTransform != null)
+        if (!fadeGemComplete)
 		{
-			Vector3 planeNormal = Vector3.Cross(target.position - transform.position, transform.right);
-			Vector3 sunVector = Vector3.ProjectOnPlane(target.position - sunTransform.position, planeNormal).normalized;
-			Vector3 holeVector = Vector3.ProjectOnPlane(target.position - hole.transform.position, planeNormal).normalized;
-			float dot = Vector3.Dot(sunVector, holeVector);
-			if (!correctTime && dot > 0.999f)
-			{
-				correctTime = true;
-				ModMain.SetCondition("SUNPOST_IN_RANGE", true);
+            if (correctTime)
+            {
+                float num = Mathf.InverseLerp(gemFadeStart, gemFadeStart + gemFadeTime, Time.time);
+                gemEmissive.SetEmissiveScale(Mathf.Lerp(lastGemFade, 1f, num));
+                if (num == 1)
+                {
+                    fadeGemComplete = true;
+                }
             }
-			else if (correctTime && dot <= 0.999f)
-			{
-				correctTime = false;
+            else
+            {
+                float num = Mathf.InverseLerp(gemFadeStart, gemFadeStart + gemFadeTime * 2f, Time.time);
+                gemEmissive.SetEmissiveScale(Mathf.Lerp(lastGemFade, 0f, num));
+                if (num == 0)
+                {
+                    fadeGemComplete = true;
+                }
+            }
+        }
+
+        if (sunTransform != null)
+        {
+            Vector3 planeNormal = Vector3.Cross(target.position - transform.position, transform.right);
+            Vector3 sunVector = Vector3.ProjectOnPlane(target.position - sunTransform.position, planeNormal).normalized;
+            Vector3 holeVector = Vector3.ProjectOnPlane(target.position - hole.transform.position, planeNormal).normalized;
+            float dot = Vector3.Dot(sunVector, holeVector);
+            if (!correctTime && dot > 0.999f)
+            {
+                correctTime = true;
+                fadeGemComplete = false;
+                lastGemFade = gemEmissive.GetEmissiveScale();
+                gemFadeStart = Time.time;
+                ModMain.SetCondition("SUNPOST_IN_RANGE", true);
+            }
+            else if (correctTime && dot <= 0.999f)
+            {
+                correctTime = false;
+                fadeGemComplete = false;
+                lastGemFade = gemEmissive.GetEmissiveScale();
+                gemFadeStart = Time.time;
                 ModMain.SetCondition("SUNPOST_IN_RANGE", false);
             }
         }
 
-		if (PlayerData.GetPersistentCondition("OPEN_SUNPOST_DOOR"))
+        if (opened && !ModMain.GetPersistentCondition("OPEN_SUNPOST_DOOR"))
 		{
-			opened = true;
-			doorController.CallOpenEvent();
+			if (!playerInHouse)
+			{
+				CloseDoor();
+			}
+			else
+			{
+                waitToClose = true;
+            }
+		}
+		else if (!opened && ModMain.GetPersistentCondition("OPEN_SUNPOST_DOOR"))
+		{
+			OpenDoor();
 		}
 	}
 
@@ -51,5 +97,32 @@ public class SunpostDetector : MonoBehaviour
 	{
 		opened = true;
 		doorController.CallOpenEvent();
+	}
+
+	private void CloseDoor()
+	{
+        opened = false;
+        doorController.CallCloseEvent();
+    }
+
+	private void OnEntry(GameObject obj)
+	{
+		if (obj.CompareTag("PlayerDetector"))
+		{
+			playerInHouse = true;
+		}
+	}
+
+	private void OnExit(GameObject obj)
+	{
+		if (obj.CompareTag("PlayerDetector"))
+		{
+			playerInHouse = false;
+			if (waitToClose)
+			{
+				opened = false;
+				doorController.CallCloseEvent();
+			}
+		}
 	}
 }
